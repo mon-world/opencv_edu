b25e5fc6102e3d031788156abbebcce5b6146e03
YANG_MON <audgns346712@gmail.com>
2021-12-30 02:53:50 +0900

    #

--- 03_cropAndResize.py
+++ 03_cropAndResize.py
@@ -1,25 +1,21 @@
-## 이미지를 자르는 것을 crop 이라 함
-## 사이즈 조절을 resize 라고 함
-
 import cv2
 
 # 1은 칼라 이미지, 0은 그레이스케일, -1은 알파채널포함한 이미지
 source = cv2.imread('data/images/sample.jpg' , 1)
-## 뒤에 1 이란 것도 칼라 가져와라 임. cv2.IMREAD_COLOR 임
-## 레퍼런스를 알고 싶으면 python opencv cv2.imread로 검색
-## 3개의 flag대신에 1, 0, -1을 사용해도 됩니다. 라고 있네.
-## 실무에서도 설명서를 보면서 이렇게 공부한다.
+# 뒤에 1 이란 것도 칼라 가져와라 임. cv2.IMREAD_COLOR 임
+# 레퍼런스를 알고 싶으면 python opencv cv2.imread로 검색
+# 3개의 flag대신에 1, 0, -1을 사용해도 됨.
 
 # 실수 값을 사용.
-## 확대와 축소 
+# 확대와 축소 
 scaleX = 0.6 # x축은 60%
 scaleY = 0.6
 
 scaleDown = cv2.resize( source, None, fx=scaleX, fy=scaleY, interpolation=cv2.INTER_LINEAR) 
-## 원하는 비율대로 조정 가능
-## 이미지, x비율, y비율 순서
-## 확대 축소 했을 떄 픽셀 유실됨. interpolation으로 채움
-## INTER_리니어 : ???
+# 원하는 비율대로 조정 가능
+# 이미지, x비율, y비율 순서
+# 확대 축소 했을 떄 픽셀 유실됨. interpolation으로 채움
+
 
 cv2.imshow('Original', source)
 cv2.imshow('Scaled Down', scaleDown)
@@ -31,7 +27,6 @@ scaleUp = cv2.resize( source, None, fx=scaleX, fy=scaleY, interpolation=cv2.INTE
 
 # cv2.imshow('Scaled Down', scaleUp)
 
-## 파이썬에서 이미지는 기본적으로 넘파이임.
 # Crop : 내가 원하는 부분만 이미지를 자르는 것!
 # 넘파이를 슬라이싱 하는것과 같다!!
 
