6e0cd73784a149ccaa63e413d3ce780107353bf4
YANG_MON <audgns346712@gmail.com>
2021-12-31 23:15:30 +0900

    erode - 이미지 침식

---
+++ 10_erode.py
@@ -0,0 +1,21 @@
+import cv2
+
+imageName = "data/images/truth.png"
+
+image = cv2.imread(imageName, 1)
+
+cv2.imshow("original", image)
+
+# 이미지 경계 축소(침식)
+
+dilationSize = 6
+
+element = cv2.getStructuringElement(cv2.MORPH_RECT, (2*dilationSize+1,2*dilationSize+1) )
+## 1.사각형으로 하련다. 2.커널사이즈
+
+imageEroded = cv2.erode(image,element)
+
+cv2.imshow("Erosion", imageEroded)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
