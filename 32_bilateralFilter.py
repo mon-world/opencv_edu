0211e4776e5632686197d9f33c1ae0c943a3ace6
YANG_MON <audgns346712@gmail.com>
2022-01-07 02:32:15 +0900

    bilateral filter

--- 32_bilateralFilter.py
+++ 32_bilateralFilter.py
@@ -1,6 +1,8 @@
 '''
 bilateral filter
 에지를 보전하면서 노이즈를 감소시킨다.
+
+픽셀에서의 값은 주변 픽셀값의 가중편균으로 대체된다.
 '''
 import cv2
 import numpy as np
@@ -10,7 +12,7 @@ img = cv2.imread('data/images/gaussian-noise.png',1 )
 result = cv2.bilateralFilter(img, 15, 80, 80 )
 # 1.이미지 2.커널 사이즈 
 # 3.sigmaColor : 클수록 이웃한 픽셀과 기준 색상의 영향이 커짐
-# 4. sigmaSpace : 커질수록 긴밀하게 주변 픽셀에 영향을 미침
+# 4.sigmaSpace : 커질수록 긴밀하게 주변 픽셀에 영향을 미침
 
 combined = np.hstack([img,result])
 
