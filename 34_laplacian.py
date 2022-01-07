1a5db9d2328bb9b73039030ee63274a48af94fe5
YANG_MON <audgns346712@gmail.com>
2022-01-07 23:37:04 +0900

    laplacian mask

--- 34_laplacian.py
+++ 34_laplacian.py
@@ -1,3 +1,11 @@
+'''
+라플라시안 필터
+[[ 0 -1  0]
+ [-1  4 -1]
+ [ 0 -1  0]]
+ 가로 세로 성분으로 물체의 edge를 출력한다.
+'''
+
 import cv2
 import numpy as np
 
@@ -6,7 +14,6 @@ img = cv2.imread('data/images/truth.png',1 )
 
 laplacian = cv2.Laplacian(img, cv2.CV_32F, ksize = 3, scale = 1)
 
-## 1.이미지 2.?? 3.커널 사이즈 4.스케일? 보통 1 쓴대
 
 cv2.imshow('lap',laplacian)
 
